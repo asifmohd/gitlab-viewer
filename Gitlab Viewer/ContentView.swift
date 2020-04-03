4bc9cac3bb50308af8d9760228a13b7f423354a4
Mohammad Asif <arshadasif0312@gmail.com>
2020-04-04 00:33:18 +0530

    Initial Commit

---
+++ Gitlab Viewer/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Gitlab Viewer
+//
+//  Created by Asif on 04/04/20.
+//  Copyright © 2020 Asif. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+        .previewDevice(PreviewDevice(rawValue: "iPhone 8"))
+    }
+}
